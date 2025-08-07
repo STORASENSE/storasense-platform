25480e141d156a26acde91b2d1910d38e21348e5
Mario Di Caprio <mariodicaprio10@gmail.com>
2025-08-07 11:15:43 +0200

    feat: frontend now differentiates PROD and DEV environments

--- frontend/src/redux/api/storaSenseApi.ts
+++ frontend/src/redux/api/storaSenseApi.ts
@@ -2,10 +2,23 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {GetMeasurementsRequest, GetMeasurementsResponse} from "@/redux/api/storaSenseApiSchemas";
 
 
+function getBaseUrl(): string {
+    switch (process.env.NODE_ENV) {
+        case "test":
+        case "development":
+            return "http://localhost:8000";
+        case "production":
+            return "http://localhost:8000";
+        default:
+            throw new Error("Environment variable NODE_ENV was not set by node environment!");
+    }
+}
+
+
 export const storaSenseApi = createApi({
     reducerPath: 'storaSenseApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:8002',
+        baseUrl: getBaseUrl(),
     }),
     endpoints: (build) => ({
 
