84aa7fb2367f38ad101c00eca0d8dcc084678fca
Ruben Nessler <ruben.nessler@outlook.com>
2025-08-15 14:06:45 +0200

    feat: added offline-icon

--- frontend/src/app/dashboard/sensors/page.tsx
+++ frontend/src/app/dashboard/sensors/page.tsx
@@ -2,36 +2,19 @@ import {FC} from "react";
 import SensorsOverview from "@/components/dashboard/sensors/SensorsOverview";
 import ProtectedPage from "@/components/ProtectedPage";
 
-// const Page: FC = () => {
-//     return (
-//         <ProtectedPage>
-//             <header className="mb-5">
-//                 <h1 className="text-3xl font-semibold text-blue-whale">
-//                     Sensors
-//                 </h1>
-//             </header>
-//             <section>
-//                 <SensorsOverview storageId={""} userId={""} />
-//             </section>
-//         </ProtectedPage>
-//     );
-// }
-
-// export default Page;
-
 const Page: FC = () => {
     return (
-        <>
+        <ProtectedPage>
             <header className="mb-5">
                 <h1 className="text-3xl font-semibold text-blue-whale">
                     Sensors
                 </h1>
             </header>
             <section>
-                <SensorsOverview storageId={""} userId={""}/>
+                <SensorsOverview storageId={""} userId={""} />
             </section>
-        </>
-);
+        </ProtectedPage>
+    );
 }
 
 export default Page;
