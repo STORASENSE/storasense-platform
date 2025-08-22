af478b777b6cb1caa0b23cf2beaa8fa2f3f184b5
Mario Di Caprio <112900250+MarioDiCaprio@users.noreply.github.com>
2025-08-22 16:00:44 +0200

    refactor: solved merge conflicts

--- frontend/src/app/dashboard/analytics/page.tsx
+++ frontend/src/app/dashboard/analytics/page.tsx
@@ -1,34 +1,37 @@
 'use client';
 
 import React from "react";
-import ProtectedPage from "@/components/ProtectedPage";
+import AuthenticationRequired from "@/components/context/AuthenticationRequired";
 import TopStatsSection from "@/components/dashboard/analytics/TopStatsSection";
 import KpiByTypeSection from "@/components/dashboard/analytics/KpiByTypeSection";
 import SensorMinMaxChart from "@/components/dashboard/analytics/SensorMinMaxChart";
 import SensorAverageChart from "@/components/dashboard/analytics/SensorAverageChart";
 import TimeWindowSelection from "@/components/dashboard/analytics/TimeWindowSelection";
+import ActiveStorageRequired from "@/components/context/ActiveStorageRequired";
 
 
 export default function Page() {
     return (
-        <ProtectedPage>
+        <AuthenticationRequired>
             <header className="mb-5">
                 <h1 className="text-3xl font-semibold text-blue-whale">
                     Analytics
                 </h1>
             </header>
-            <section className="space-y-6">
-                {/* Window Switcher */}
-                <TimeWindowSelection />´
-                {/* Top stats */}
-                <TopStatsSection />´
-                {/* KPI Cards by type */}
-                <KpiByTypeSection />´
-                {/* Min/Max per sensor */}
-                <SensorMinMaxChart />
-                {/* Averages by sensor type */}
-                <SensorAverageChart />
-            </section>
-        </ProtectedPage>
+            <ActiveStorageRequired>
+                <section className="space-y-6">
+                    {/* Window Switcher */}
+                    <TimeWindowSelection />´
+                    {/* Top stats */}
+                    <TopStatsSection />´
+                    {/* KPI Cards by type */}
+                    <KpiByTypeSection />´
+                    {/* Min/Max per sensor */}
+                    <SensorMinMaxChart />
+                    {/* Averages by sensor type */}
+                    <SensorAverageChart />
+                </section>
+            </ActiveStorageRequired>
+        </AuthenticationRequired>
     );
 }
