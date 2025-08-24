8aad0f660996fcd41bd8d7d7345aab4b6901f5ee
Ruben Nessler <ruben.nessler@outlook.com>
2025-08-24 21:25:31 +0200

    fix: refactored mqtt-client auth process

---
+++ Alarm-Service/app/src/main/java/alarm/model/Treshold.java
@@ -0,0 +1,14 @@
+package main.java.alarm.model;
+
+public class Treshold {
+    public double min;
+    public double max;
+
+    public Threshold() {}
+
+    public Threshold(double min, double max) {
+    this.min = min;
+    this.max = max;
+}
+
+}
